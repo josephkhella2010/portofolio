c42c3c82e3b607f3e6c2970800168a2436ae1bb3
joseph khella <josephkhella2010@gmail.com>
2025-02-02 17:56:11 +0100

    adjust skill section

--- frontend/portfolio/src/pages/skills/WebExperienceSection.tsx
+++ frontend/portfolio/src/pages/skills/WebExperienceSection.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+/* import React, { useEffect, useState } from "react";
 import styles from "./skill.module.css";
-
+interface WebSkillType {
+  name: string;
+  scale: number;
+}
 export default function WebExperienceSection() {
   const webSkill: WebSkillType[] = [
     { name: "HTML", scale: 100 },
@@ -39,12 +42,6 @@ export default function WebExperienceSection() {
     return () => clearInterval(interval);
   }, [progressValue, webSkill]);
 
-  /*  function handleSkill(val: string) {
-    if (val === "skill") {
-      setProgressValue([0, 0, 0]);
-    }
-    setSkills(val);
-  } */
   return (
     <div className={styles.progressWrapper}>
       {webSkill.map((item, index) => (
@@ -59,6 +56,77 @@ export default function WebExperienceSection() {
             <div className={styles.valueContainer}>
               <p>{item.name}</p>
               <p>{progressValue[index]}%</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+ */
+import React, { useEffect, useMemo, useState } from "react";
+import styles from "./skill.module.css";
+
+interface WebSkillType {
+  name: string;
+  scale: number;
+}
+
+export default function WebExperienceSection() {
+  const webSkill: WebSkillType[] = useMemo(
+    () => [
+      { name: "HTML", scale: 100 },
+      { name: "CSS", scale: 95 },
+      { name: "JS", scale: 90 },
+      { name: "React", scale: 85 },
+      { name: "Rest Api", scale: 86 },
+      { name: "Sass", scale: 87 },
+      { name: "Figma", scale: 82 },
+      { name: "Nodejs", scale: 65 },
+      { name: "NextJs", scale: 65 },
+      { name: "Python", scale: 60 }
+    ],
+    []
+  );
+
+  const [progressVal, setProgressVal] = useState<number[]>(
+    Array(webSkill.length).fill(0)
+  );
+
+  console.log(progressVal);
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      const upgradeValue = [...progressVal];
+      for (let i = 0; i < webSkill.length; i++) {
+        //console.log(progressVal[i]);
+        if (upgradeValue[i] < webSkill[i].scale) {
+          upgradeValue[i] += 1;
+        } else {
+          upgradeValue[i] = webSkill[i].scale;
+        }
+      }
+      setProgressVal(upgradeValue);
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, [progressVal, webSkill]);
+
+  return (
+    <div className={styles.progressWrapper}>
+      {webSkill.map((item, index) => (
+        <div key={index} className={styles.circle}>
+          <div
+            className={styles.progress}
+            style={{
+              background: `conic-gradient(rgb(44 69 85) ${
+                (progressVal[index] * 360) / 100
+              }deg,rgb(101 102 103 / 94%) ${
+                (progressVal[index] * 360) / 100
+              }deg)`
+            }}>
+            <div className={styles.valueContainer}>
+              <p>{item.name}</p>
+              <p>{progressVal[index]}%</p>
             </div>
           </div>
         </div>
