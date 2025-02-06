d02277e72c049258e4a9df2a0af1e283d51f9ef2
joseph khella <josephkhella2010@gmail.com>
2025-02-06 18:33:55 +0100

    download updated resume

--- frontend/portfolio/src/components/DownloadBtn.tsx
+++ frontend/portfolio/src/components/DownloadBtn.tsx
@@ -4,8 +4,8 @@ import { FaDownload } from "react-icons/fa6";
 export default function DownloadBtn() {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/cv3.pdf";
-    link.download = "cv3.pdf";
+    link.href = `/resume.pdf`;
+    link.download = "resume.pdf";
     link.title = "Download resume";
     document.body.appendChild(link);
     link.click();
