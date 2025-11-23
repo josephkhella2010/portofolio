0e247de0d9541b37315467614edd757be3f6b98a
Joseph Khella <josephkhella2010@gmail.com>
2025-11-23 22:12:47 +0100

    fix eleven

--- frontend/portfolio/src/pages/skills/WebExperienceSection.tsx
+++ frontend/portfolio/src/pages/skills/WebExperienceSection.tsx
@@ -276,14 +276,14 @@ export default function WebExperienceSection() {
       rgb(67 54 84) ${(progressVal[index] * 360) / 100}deg,
       rgb(101 102 103 / 94%) ${(progressVal[index] * 360) / 100}deg
     )`,
-              boxShadow: `
+              /*            boxShadow: `
       0 0 25px rgba(67,54,84,0.9),
       0 0 30px rgba(101,102,103,0.6),
       inset 0 0 25px rgba(67,54,84,0.7),
       inset 0 0 30px rgba(101,102,103,0.6)
     `,
               filter: "brightness(1.35) saturate(1.45)",
-              borderRadius: "50%",
+              borderRadius: "50%", */
             }}
           >
             <div className={styles.valueContainer}>
