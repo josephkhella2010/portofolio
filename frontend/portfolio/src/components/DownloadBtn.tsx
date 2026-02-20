9c29ebdb5fd6e82a1a12bf78963ee32e46c28b19
Joseph Khella <josephkhella2010@gmail.com>
2026-02-20 19:43:01 +0100

    english or swedish  cv

--- frontend/portfolio/src/components/DownloadBtn.tsx
+++ frontend/portfolio/src/components/DownloadBtn.tsx
@@ -1,31 +1,9 @@
-/* import React from "react";
+/* 
 import { FaDownload } from "react-icons/fa6";
+import { useLanguage } from "../context/LanguageContext";
 
 export default function DownloadBtn() {
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href = `/resume.pdf`;
-    link.download = "resume.pdf";
-    link.title = "Download resume";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-  return (
-    <div className="downloadBtnContainer">
-      <button
-        onClick={handleDownload}
-        className="downloadBtn"
-        title="Download resume">
-        Download CV <FaDownload />
-      </button>
-    </div>
-  );
-}
- */
-import { FaDownload } from "react-icons/fa6";
-
-export default function DownloadBtn() {
+   const { language } = useLanguage();
   const handleDownload = () => {
     const link = document.createElement("a");
     link.href = `/resume.pdf`;
@@ -43,6 +21,36 @@ export default function DownloadBtn() {
         title="Download resume"
       >
         Download CV <FaDownload />
+      </button>
+    </div>
+  );
+}
+ */
+
+import { FaDownload } from "react-icons/fa6";
+import { useLanguage } from "../context/LanguageContext";
+
+export default function DownloadBtn() {
+  const { language } = useLanguage();
+  const handleDownload = () => {
+    const fileName =
+      language === "sv" ? "resume-swedish.pdf" : "resume-english.pdf";
+    const link = document.createElement("a");
+    link.href = `/${fileName}`;
+    link.download = fileName;
+    link.title = "Download resume";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  return (
+    <div className="downloadBtnContainer">
+      <button
+        onClick={handleDownload}
+        className="downloadBtn"
+        title="Download resume"
+      >
+        {language === "sv" ? "Ladda ner" : "download"} CV <FaDownload />
       </button>
     </div>
   );
